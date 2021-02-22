1e9e25280f147c1340de61534e9b2c7fc9c8075f
LunarDiver <25Prozent@gmail.com>
2021-02-22 14:26:17 +0100

    Added new project for automatic db moving, optimized csproj files.

---
+++ DbPreparation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DbPreparation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
